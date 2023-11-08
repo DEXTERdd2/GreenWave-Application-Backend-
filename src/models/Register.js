66611e44577094b4ffe1281c28e2966ce8514e06
DEXTERdd2 <faizanshahid127@gmail.com>
2023-11-09 01:03:48 +0500

    Register model changes

--- src/models/Register.js
+++ src/models/Register.js
@@ -25,7 +25,35 @@ password: {
 phoneNumber: {
     type: String,
     required: true
-}
+},
+followers: {
+    type: Array,
+    required: true,
+  },
+  points: {
+    type: Number,
+    default: 0,
+  },
+  following: {
+    type: Array,
+    required: true,
+  },
+  blockedUsers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "NewUsers",
+    },
+  ],
+  blockedByUsers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "NewUsers",
+    },
+  ],
+  location: {},
+  expoPushToken: {
+    type: String,
+  },
 });
 
 const NewUsers = new mongoose.model("NewUsers", dbSchema);
